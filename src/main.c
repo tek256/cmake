f7feee52c61ef73386e0b6bc8cd284ca445f11d5
Devon <devon@tek256.com>
2020-11-03 23:10:45 -0800

    Cleaned repository

--- src/main.c
+++ src/main.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-  int value = 0;
-
-  printf("What is the answer to the Universe and everything?\n");
-
-  // Get a value from the command line/stdin
-  fscanf(stdin, "%i", &value);
-
-  if (value == 42) {
-    printf("Correct!\n");
-  } else {
-    printf("What a shame, you're wrong.\n");
-  }
-
+  printf("What is the answer to the Universe and everything? 42.\n");
   return 0;
 }
